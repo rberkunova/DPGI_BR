6e0b641c3873eb6a600cac0ea391d808440b732a
rberkunova <rberkunova@gmail.com>
2025-05-26 19:31:01 +0300

    завдання 4 пункт 7-16

--- lab2/MainWindow.xaml.cs
+++ lab2/MainWindow.xaml.cs
@@ -1,24 +1,80 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace lab2
+namespace TextEditorApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Save = new RoutedUICommand(
+            "Save", "Save", typeof(CustomCommands),
+            new InputGestureCollection() { new KeyGesture(Key.S, ModifierKeys.Control) });
+
+        public static readonly RoutedUICommand Open = new RoutedUICommand(
+            "Open", "Open", typeof(CustomCommands),
+            new InputGestureCollection() { new KeyGesture(Key.O, ModifierKeys.Control) });
+
+        public static readonly RoutedUICommand Clear = new RoutedUICommand(
+            "Clear", "Clear", typeof(CustomCommands));
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBinding saveBinding = new CommandBinding(CustomCommands.Save, Executed_Save, CanExecute_Save);
+            this.CommandBindings.Add(saveBinding);
+
+            CommandBinding openBinding = new CommandBinding(CustomCommands.Open, Executed_Open, CanExecute_Open);
+            this.CommandBindings.Add(openBinding);
+
+            CommandBinding clearBinding = new CommandBinding(CustomCommands.Clear, Executed_Clear, CanExecute_Clear);
+            this.CommandBindings.Add(clearBinding);
+        }
+
+        private void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (mainTextBox != null && mainTextBox.Text.Length > 0) e.CanExecute = true;
+            else e.CanExecute = false;
+        }
+
+        private void Executed_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, mainTextBox.Text);
+            }
+        }
+
+        private void CanExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Executed_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mainTextBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void CanExecute_Clear(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (mainTextBox != null && mainTextBox.Text.Length > 0) e.CanExecute = true;
+            else e.CanExecute = false;
+        }
+
+        private void Executed_Clear(object sender, ExecutedRoutedEventArgs e)
+        {
+            mainTextBox.Clear();
         }
     }
 }
